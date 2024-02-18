d227c6ad96e84234856313cd89e7bdd02a1fdf9b
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-02-18 20:32:07 +0300

    Add User class class parameters: name, age. Initialized User object

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,6 @@
 public class HelloWorld{
 public static void main(Stirng[] args)
+User user = new User();
+user.name = "Ann";
+user.age = 25;
 }
---
+++ User.java
@@ -0,0 +1,4 @@
+public class User{
+	public String name;
+	public int age;
+}
